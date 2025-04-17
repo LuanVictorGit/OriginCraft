3d6f7b13fd375c8e4692e97db75964fba0224984
LuanVictorGit <luanvictorchagas2015@gmail.com>
2025-04-17 17:22:39 -0300

    updated buttons category

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-clickedIp=false;
+let clickedIp = false;
+let categorySelected;
+
+document.addEventListener("DOMContentLoaded", ()=>{
+
+    categorySelected = document.getElementById("vip1");
+    document.addEventListener("click", (e) => {
+        if (!e.target.classList.contains("button_category")) return;
+        e.preventDefault();
+        const element = e.target;
+        if (!element.classList.contains("category_selected")){
+            categorySelected.classList.remove("category_selected");
+            element.classList.add("category_selected");
+            categorySelected = element;
+        }
+    });
+
+});
